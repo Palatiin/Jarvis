74c321821ced990f0e0a4171e15ecf3c89cf6c73
Palatiin <matoremen@gmail.com>
2021-07-01 13:34:42 +0200

    module_validator: Added platform validation

--- module_validator.py
+++ module_validator.py
@@ -6,12 +6,28 @@ import re
 
 ## Static analysis of module's header
 #  all specifics that have to be met are specified in comment of class Module
-# @param module name of module source file (module.py)
+# @param module path to module source file (module.py)
+# @param platform Operting system wich must be supported by module
 # @return Logical value, whether module's structure is valid
-def validate_module(module: str) -> bool:
-    is_valid = True
+def validate_module(module: str, platform: str) -> bool:
+    is_valid = False 
+    platform_assign_pattern = "\s+self.platform *= *\[([\"\', \w]+)\]"
+    platform_append_pattern = "\s+self.platform.append\(([\"\', \w]+)\)"
 
-    # TODO
+    with open(module, "r") as f:
+        data = "".join(f.readlines())
+        
+        # checking platform compatibility
+        assign_pattern_result = re.search(platform_assign_pattern, data)
+        append_pattern_result = re.findall(platform_append_pattern, data)
+        if assign_pattern_result and platform in assign_pattern_result.group(1):
+            is_valid = True
+        elif append_pattern_result:
+            for result in append_pattern_result:
+                if platform in result:
+                    is_valid = True
+
+        # TODO
 
     return is_valid
 
@@ -40,7 +56,7 @@ def load_modules(platform: str, directory: str = "modules/") -> list:
 
         module_source_path = f"./{directory}{dirname}/{dirname}.py"
 
-        if validate_module(module_source_path):
+        if validate_module(module_source_path, platform):
             valid_modules.append(dirname)
 
     return valid_modules
